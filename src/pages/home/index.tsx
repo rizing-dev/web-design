b0efa9f8c70327508617b2eb79051d14ca59e984
Jitendra Dhandiya <jitendra@example.com>
2025-03-16 11:11:19 -0400

    second commit

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const TextHome = () => {
-  return <div>Text  Home Heyyy!!! Thats the Test Home Page</div>;
+  return <div>
+        <h1>Welcome To Our Application</h1>
+    Text  Home Heyyy!!! Thats the Test Home Page
+  </div>;
 };
 
 export default TextHome;
