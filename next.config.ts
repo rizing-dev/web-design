8f9e0bd77a31fc95e18c3d50c769d568983dfc46
Alexander The Great <great@alexander.com>
2025-11-21 22:21:45 +0530

    updated

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,12 @@ const nextConfig: NextConfig = {
   devIndicators: false,
   webpack(config) {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    
+    // Disable caching to fix server build issues
+    if (!config.cache) {
+      config.cache = false;
+    }
+
     return config;
   },
 };
