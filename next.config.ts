553dcea2b52f6cd732e1606578fbdb23f707c101
Alexander The Great <great@alexander.com>
2025-11-21 22:16:02 +0530

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
-  devIndicators: false
+  devIndicators: false,
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 export default nextConfig;
